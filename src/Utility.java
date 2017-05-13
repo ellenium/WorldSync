1006f85d34903fc93ec3fb4622668b3493827a45
Ellen Hebert <ellenhebert@icloud.com>
2017-05-13 12:07:39 -0400

    Create GUI package and move GUI classes there

--- src/Utility.java
+++ src/Utility.java
@@ -1,5 +1,9 @@
 public class Utility {
 
+    /**
+     * Returns the location of the user's application data folder.
+     * It is located in AppData/Roaming in windows.
+     */
     public static String getAppdataFolder() {
         return "";
     }
