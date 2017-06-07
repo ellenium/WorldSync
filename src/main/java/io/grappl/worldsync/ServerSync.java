746dff16cc60181d366725972c7885694660c487
Ellen Hebert <ellenhebert@icloud.com>
2017-06-07 10:59:42 -0400

    Move methods from PackAndShip into ServerSync because they seemed like they belonged there more; delete PackAndShip

--- src/main/java/io/grappl/worldsync/ServerSync.java
+++ src/main/java/io/grappl/worldsync/ServerSync.java
@@ -6,14 +6,18 @@ import io.grappl.client.impl.ApplicationState;
 import io.grappl.client.impl.GrapplBuilder;
 import io.grappl.client.impl.error.RelayServerNotFoundException;
 import io.grappl.worldsync.gui.MainGUI;
-import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.*;
 import java.io.*;
-import java.util.UUID;
+import java.net.Socket;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
- * The main class. This runs when the program starts.
+ * The main class.
+ * This is the class that should be called when the .jar
+ * is run. It will instantly construct a MainGUI, which
+ * contains the log-in prompt.
  */
 public class ServerSync {
 
@@ -132,4 +136,102 @@ public class ServerSync {
         }
     }
     // Server object will send ServerUpdate messages to core to update the servers status on occasion
+
+    public static void pack(String serverName) {
+        String dataFolder = Utility.getAppdataFolder() + "servers\\";
+        String serverFolder = dataFolder + serverName + "\\";
+
+        File theZip = new File(Utility.getAppdataFolder() + "zips\\" + serverName + ".zip");
+
+        try {
+            theZip.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(theZip));
+            recurse(serverFolder, "", zipOutputStream);
+            zipOutputStream.flush();
+            zipOutputStream.closeEntry();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void recurse(String worldName, String name, ZipOutputStream zipOutputStream) {
+//        System.out.println("Checking: " + name);
+        File theFolder = new File(worldName + name);
+
+        if (theFolder.listFiles().length == 0) return;
+
+        for (File f : theFolder.listFiles()) {
+            ZipEntry zipEntry = new ZipEntry(name + "" + f.getName());
+            try {
+                zipOutputStream.putNextEntry(zipEntry);
+                if (!f.isDirectory()) {
+                    FileInputStream fileInputStream = new FileInputStream(f);
+                    org.apache.commons.io.IOUtils.copy(fileInputStream, zipOutputStream);
+                }
+                zipOutputStream.flush();
+                zipOutputStream.closeEntry();
+                System.out.println("Put entry: " + (name + "" + f.getName()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (f.isDirectory()) {
+                recurse(worldName, name + f.getName() + "/", zipOutputStream);
+            }
+        }
+    }
+
+    public static void ship(File zip, String name) {
+        System.out.println("uploading");
+        String ip = "localhost";
+        int port = 33233;
+
+        try {
+            Socket socket = new Socket(ip, port);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeInt(1);
+            PrintStream printStream = new PrintStream(dataOutputStream);
+            printStream.println(name);
+            dataOutputStream.flush();
+            System.out.println("Name sent");
+            FileInputStream fileInputStream = new FileInputStream(zip);
+            org.apache.commons.io.IOUtils.copy(fileInputStream, socket.getOutputStream());
+            socket.getOutputStream().flush();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done");
+    }
+
+    public static void retrieve(File zip, String name) {
+        System.out.println("Retrieving");
+        String ip = "localhost";
+        int port = 33233;
+
+        try {
+            Socket socket = new Socket(ip, port);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeInt(2);
+            PrintStream printStream = new PrintStream(dataOutputStream);
+            printStream.println(name);
+            System.out.println("Name sent");
+            FileOutputStream fileOutputStream = new FileOutputStream(zip);
+            org.apache.commons.io.IOUtils.copy(socket.getInputStream(), fileOutputStream);
+            fileOutputStream.flush();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done");
+    }
 }
